20fcd1e25bb5bbedebabb777a121d51e1b279c6a
Magnus Eriksson <eriksson.mag@gmail.com>
2021-07-10 18:08:37 +0200

    Refactor font names

--- src/fonts.rs
+++ src/fonts.rs
@@ -9,25 +9,25 @@ pub fn get_font<'a>(fonts: &'a HashMap<String, Font>, name: &str) -> &'a Font {
 pub fn load_fonts(rl: &mut RaylibHandle, thread: &RaylibThread) -> HashMap<String, Font> {
     let mut fonts = HashMap::<String, Font>::new();
 
-    let calibrib = rl.load_font_ex(&thread, "resources/calibrib.ttf", 50, FontLoadEx::Default(0))
+    let calibri_12 = rl.load_font_ex(&thread, "resources/calibri.ttf", 13, FontLoadEx::Default(0))
         .expect("Failed to get font");
-    let calibrib2 = rl.load_font_ex(&thread, "resources/calibrib.ttf", 25, FontLoadEx::Default(0))
+    let calibri_20 = rl.load_font_ex(&thread, "resources/calibri.ttf", 20, FontLoadEx::Default(0))
         .expect("Failed to get font");
-    let calibri = rl.load_font_ex(&thread, "resources/calibri.ttf", 20, FontLoadEx::Default(0))
+    let calibri_30 = rl.load_font_ex(&thread, "resources/calibri.ttf", 30, FontLoadEx::Default(0))
         .expect("Failed to get font");
-    let calibril = rl.load_font_ex(&thread, "resources/calibri.ttf", 30, FontLoadEx::Default(0))
+    let calibri_25_bold = rl.load_font_ex(&thread, "resources/calibrib.ttf", 25, FontLoadEx::Default(0))
         .expect("Failed to get font");
-    let calibri_bold_40 = rl.load_font_ex(&thread, "resources/calibrib.ttf", 40, FontLoadEx::Default(0))
+    let calibri_40_bold = rl.load_font_ex(&thread, "resources/calibrib.ttf", 40, FontLoadEx::Default(0))
         .expect("Failed to get font");
-    let calibri_12 = rl.load_font_ex(&thread, "resources/calibri.ttf", 12, FontLoadEx::Default(0))
+    let calibri_50_bold = rl.load_font_ex(&thread, "resources/calibrib.ttf", 50, FontLoadEx::Default(0))
         .expect("Failed to get font");
 
-    fonts.insert("calibrib".to_string(), calibrib);
-    fonts.insert("calibrib2".to_string(), calibrib2);
-    fonts.insert("calibri".to_string(), calibri);
-    fonts.insert("calibril".to_string(), calibril);
-    fonts.insert("calibri_bold_40".to_string(), calibri_bold_40);
     fonts.insert("calibri_12".to_string(), calibri_12);
+    fonts.insert("calibri_20".to_string(), calibri_20);
+    fonts.insert("calibri_30".to_string(), calibri_30);
+    fonts.insert("calibri_25_bold".to_string(), calibri_25_bold);
+    fonts.insert("calibri_40_bold".to_string(), calibri_40_bold);
+    fonts.insert("calibri_50_bold".to_string(), calibri_50_bold);
 
     return fonts;
 }
--- src/widgets.rs
+++ src/widgets.rs
@@ -23,14 +23,14 @@ pub fn draw_cpu_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Hash
     let cpu_package_temp: f32 = latest_data.values.get("cpu_package_temp").unwrap_or(&"0".to_string()).parse().unwrap();
     let cpu_power: f32 = latest_data.values.get("cpu_power").unwrap_or(&"0".to_string()).parse().unwrap();
 
-    d.draw_text_ex(get_font(fonts, "calibrib"), "CPU", Vector2::new(xf + 10.0, yf + 10.0), 50.0, 0.0, Color::WHITE);
-    d.draw_text_ex(get_font(fonts, "calibri"), &*format!("{:.2} W", cpu_power), Vector2::new(xf + 110.0, yf + 21.0), 20.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_50_bold"), "CPU", Vector2::new(xf + 10.0, yf + 10.0), 50.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("{:.2} W", cpu_power), Vector2::new(xf + 110.0, yf + 21.0), 20.0, 0.0, Color::WHITE);
 
-    draw_temperature_gauge(&mut d, x + 200, y + 5, cpu_die_temp as i32, get_font(fonts, "calibri"), get_font(fonts, "calibri_12"));
-    draw_temperature_gauge(&mut d, x + 275, y + 5, cpu_package_temp as i32, get_font(fonts, "calibri"), get_font(fonts, "calibri_12"));
-    d.draw_text_ex(get_font(fonts, "calibril"), &*format!("{} MHz", max_core_frequency), Vector2::new(xf + 340.0, yf + 18.0), 30.0, 0.0, Color::WHITE);
+    draw_temperature_gauge(&mut d, x + 200, y + 5, cpu_die_temp as i32, get_font(fonts, "calibri_20"), get_font(fonts, "calibri_12"));
+    draw_temperature_gauge(&mut d, x + 275, y + 5, cpu_package_temp as i32, get_font(fonts, "calibri_20"), get_font(fonts, "calibri_12"));
+    d.draw_text_ex(get_font(fonts, "calibri_30"), &*format!("{} MHz", max_core_frequency), Vector2::new(xf + 340.0, yf + 18.0), 30.0, 0.0, Color::WHITE);
 
-    d.draw_text_ex(get_font(fonts, "calibrib2"), "Usage", Vector2::new(xf + 10.0, yf + 65.0), 25.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_25_bold"), "Usage", Vector2::new(xf + 10.0, yf + 65.0), 25.0, 0.0, Color::WHITE);
     let gradient_color_1 = Color::new(0, 200, 0, 255);
     let gradient_color_2 = Color::new(0, 40, 0, 255);
     draw_meter_bar(&mut d, x + 80, y + 65, 390, 23, cpu_utilization as i32, 100, (gradient_color_1, gradient_color_2), fonts);
@@ -61,18 +61,18 @@ pub fn draw_gpu_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Hash
     let gpu_frequency: f32 = latest_data.values.get("gpu_frequency").unwrap_or(&"0".to_string()).parse().unwrap();
     let gpu_fps: f32 = latest_data.values.get("gpu_fps").unwrap_or(&"0".to_string()).parse().unwrap();
 
-    d.draw_text_ex(get_font(fonts, "calibrib"), "GPU", Vector2::new(xf + 10.0, yf + 10.0), 50.0, 0.0, Color::WHITE);
-    d.draw_text_ex(get_font(fonts, "calibri"), &*format!("{:.2} W", gpu_power), Vector2::new(xf + 110.0,  yf + 12.0), 20.0, 0.0, Color::WHITE);
-    d.draw_text_ex(get_font(fonts, "calibri"), &*format!("{:.2} V", gpu_voltage), Vector2::new(xf + 110.0, yf + 30.0), 20.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_50_bold"), "GPU", Vector2::new(xf + 10.0, yf + 10.0), 50.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("{:.2} W", gpu_power), Vector2::new(xf + 110.0,  yf + 12.0), 20.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("{:.2} V", gpu_voltage), Vector2::new(xf + 110.0, yf + 30.0), 20.0, 0.0, Color::WHITE);
 
-    draw_temperature_gauge(&mut d, x + 200, y + 5, gpu_die_temp as i32, get_font(fonts, "calibri"), get_font(fonts, "calibri_12"));
-    draw_temperature_gauge(&mut d, x + 275, y + 5, gpu_package_temp as i32, get_font(fonts, "calibri"), get_font(fonts, "calibri_12"));
+    draw_temperature_gauge(&mut d, x + 200, y + 5, gpu_die_temp as i32, get_font(fonts, "calibri_20"), get_font(fonts, "calibri_12"));
+    draw_temperature_gauge(&mut d, x + 275, y + 5, gpu_package_temp as i32, get_font(fonts, "calibri_20"), get_font(fonts, "calibri_12"));
 
-    d.draw_text_ex(get_font(fonts, "calibril"), &*format!("{} MHz", gpu_frequency), Vector2::new(xf + 340.0, yf + 3.0), 30.0, 0.0, Color::WHITE);
-    d.draw_text_ex(get_font(fonts, "calibril"), &*format!("{} FPS", gpu_fps), Vector2::new(xf + 340.0, yf + 32.0), 30.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_30"), &*format!("{} MHz", gpu_frequency), Vector2::new(xf + 340.0, yf + 3.0), 30.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_30"), &*format!("{} FPS", gpu_fps), Vector2::new(xf + 340.0, yf + 32.0), 30.0, 0.0, Color::WHITE);
     let gradient_color_1 = Color::new(200, 0, 0, 255);
     let gradient_color_2 = Color::new(40, 0, 0, 255);
-    d.draw_text_ex(get_font(fonts, "calibrib2"), "Usage", Vector2::new(xf + 10.0, yf + 65.0), 25.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_25_bold"), "Usage", Vector2::new(xf + 10.0, yf + 65.0), 25.0, 0.0, Color::WHITE);
     draw_meter_bar(&mut d, x + 80, y + 65, 390, 23, gpu_utilization as i32, 100, (gradient_color_1, gradient_color_2), fonts);
 
     draw_graph_grid(&mut d, x + 10, y + 100);
@@ -96,13 +96,13 @@ pub fn draw_mem_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Hash
     let mem_available : f32 = latest_data.values.get("mem_available").unwrap_or(&"0".to_string()).parse().unwrap();
     let mem_used_percent = mem_used / (mem_used + mem_available);
 
-    d.draw_text_ex(get_font(fonts, "calibri_bold_40"), "Mem", Vector2::new(xf + 10.0, yf + 10.0), 40.0, 0.0, Color::WHITE);
-    d.draw_text_ex(get_font(fonts, "calibri"), &*format!("Available: {:.2} GB", mem_available), Vector2::new(xf + 110.0,  yf + 12.0), 20.0, 0.0, Color::WHITE);
-    d.draw_text_ex(get_font(fonts, "calibri"), &*format!("Used: {:.2} GB", mem_used), Vector2::new(xf + 141.0,  yf + 30.0), 20.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_40_bold"), "Mem", Vector2::new(xf + 10.0, yf + 10.0), 40.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("Available: {:.2} GB", mem_available), Vector2::new(xf + 110.0,  yf + 12.0), 20.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("Used: {:.2} GB", mem_used), Vector2::new(xf + 141.0,  yf + 30.0), 20.0, 0.0, Color::WHITE);
 
     let gradient_color_1 = Color::new(50, 50, 255, 255);
     let gradient_color_2 = Color::new(10, 10, 50, 255);
-    d.draw_text_ex(get_font(fonts, "calibrib2"), "Usage", Vector2::new(xf + 10.0, yf + 65.0), 25.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_25_bold"), "Usage", Vector2::new(xf + 10.0, yf + 65.0), 25.0, 0.0, Color::WHITE);
     draw_meter_bar(&mut d, x + 80, y + 65, 390, 23, (mem_used_percent * 100.0) as i32, 100, (gradient_color_1, gradient_color_2), fonts);
 
     draw_graph_grid(&mut d, x + 10, y + 100);
@@ -123,13 +123,13 @@ pub fn draw_core_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Has
 
     let latest_data = &data[data.len() - 1];
 
-    d.draw_text_ex(get_font(fonts, "calibrib"), "CPU Cores", Vector2::new(x as f32, y as f32 + 10.0), 50.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_50_bold"), "CPU Cores", Vector2::new(x as f32, y as f32 + 10.0), 50.0, 0.0, Color::WHITE);
     for core in 1..9 {
         let core_load: f32 = latest_data.values.get(&*format!("cpu_core_load_{}", core)).unwrap_or(&"0".to_string()).parse().unwrap();
         let core_frequency: f32 = latest_data.values.get(&*format!("cpu_core_frequency_{}", core)).unwrap_or(&"0".to_string()).parse().unwrap();
         let core_y = y + (core - 1) * 28 + 65;
         let core_frequency = format!("{:.0} MHz", core_frequency);
-        d.draw_text_ex(get_font(fonts, "calibri"), &*format!("#{}", core), Vector2::new(x as f32, core_y as f32), 20.0, 0.0, Color::WHITE);
+        d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("#{}", core), Vector2::new(x as f32, core_y as f32), 20.0, 0.0, Color::WHITE);
         draw_meter_bar_with_label(&mut d, x + 30, core_y, 195, 23, core_load as i32, 100, (gradient_color_1, gradient_color_2), fonts, core_frequency, 60.0, Color::WHITE);
     }
     for core in 9..17 {
@@ -137,7 +137,7 @@ pub fn draw_core_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Has
         let core_frequency: f32 = latest_data.values.get(&*format!("cpu_core_frequency_{}", core)).unwrap_or(&"0".to_string()).parse().unwrap();
         let core_y = y + (core - 9) * 28 + 65;
         let core_frequency = format!("{:.0} MHz", core_frequency);
-        d.draw_text_ex(get_font(fonts, "calibri"), &*format!("#{}", core), Vector2::new(x as f32 + 240.0, core_y as f32), 20.0, 0.0, Color::WHITE);
+        d.draw_text_ex(get_font(fonts, "calibri_20"), &*format!("#{}", core), Vector2::new(x as f32 + 240.0, core_y as f32), 20.0, 0.0, Color::WHITE);
         draw_meter_bar_with_label(&mut d, x + 275, core_y, 195, 23, core_load as i32, 100, (gradient_color_1, gradient_color_2), fonts, core_frequency, 60.0, Color::WHITE);
     }
 }
@@ -146,7 +146,7 @@ pub fn draw_hdd_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Hash
 {
     let latest_data = &data[data.len() - 1];
 
-    d.draw_text_ex(get_font(fonts, "calibrib"), "HDD", Vector2::new(x as f32, y as f32), 50.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_50_bold"), "HDD", Vector2::new(x as f32, y as f32), 50.0, 0.0, Color::WHITE);
 
     for i in 1..=6 {
         let drive_name = latest_data.values.get(&format!("hdd_drive_name_{}", i));
@@ -156,15 +156,15 @@ pub fn draw_hdd_panel(mut d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &Hash
 
         if drive_name.is_some() {
             let label = format!("Free: {} GB / {} GB", drive_free / 1000000000, drive_total / 1000000000);
-            d.draw_text_ex(get_font(fonts, "calibri"), drive_name.unwrap_or(&"?".to_string()), Vector2::new((x + 3) as f32, (y + (i - 1) * 30 + 2) as f32 + 50 as f32), 20.0, 0.0, Color::WHITE);
-            draw_meter_bar_with_label(&mut d, x + 25, y + (i - 1) * 30 + 50, 438, 23, (((drive_total - drive_free) as f64 / drive_total as f64) * 100.0 as f64) as i32, 100, (Color::DARKPURPLE, Color::BLACK), fonts, label, 130.0, Color::WHITE);
+            d.draw_text_ex(get_font(fonts, "calibri_20"), drive_name.unwrap_or(&"?".to_string()), Vector2::new((x + 3) as f32, (y + (i - 1) * 30 + 2) as f32 + 50 as f32), 20.0, 0.0, Color::WHITE);
+            draw_meter_bar_with_label(&mut d, x + 25, y + (i - 1) * 30 + 50, 438, 23, (((drive_total - drive_free) as f64 / drive_total as f64) * 100.0 as f64) as i32, 100, (Color::VIOLET, Color::BLACK), fonts, label, 130.0, Color::WHITE);
         }
     }
 }
 
 pub fn draw_time_panel(d: &mut RaylibDrawHandle, x: i32, y: i32, fonts: &HashMap<String, Font>) {
     let date = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-    d.draw_text_ex(get_font(fonts, "calibri"), &date, Vector2::new(x as f32, y as f32), 20.0, 0.0, Color::WHITE);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &date, Vector2::new(x as f32, y as f32), 20.0, 0.0, Color::WHITE);
 }
 
 pub fn draw_graph_grid(d: &mut &mut RaylibDrawHandle, x: i32, y: i32) {
@@ -211,7 +211,7 @@ pub fn draw_meter_bar_with_label(d: &mut &mut RaylibDrawHandle, x: i32, y: i32, 
     let bar_width = width * value / max_value;
     d.draw_rectangle_gradient_v(x + 1, y + 1, bar_width, height - 2, color.0, color.1);
 
-    d.draw_text_ex(get_font(fonts, "calibri"), &label, Vector2::new(label_pos + x as f32, y as f32 + 3.0), 20.0, 0.0, label_color);
+    d.draw_text_ex(get_font(fonts, "calibri_20"), &label, Vector2::new(label_pos + x as f32, y as f32 + 3.0), 20.0, 0.0, label_color);
 }
 
 pub fn draw_temperature_gauge(d: &mut RaylibDrawHandle, x: i32, y: i32, value: i32, font: &Font, font2: &Font) {
